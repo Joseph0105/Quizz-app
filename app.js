d40cd8dd924a61aea462601ed12b7e7e2a7bfaf3
joseph_dev <joseph.schneider0105@gmail.com>
2023-02-02 16:58:25 -0500

    début du js, récupération des résults true ou false en console

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 const responses = ["c", "a", "b", "a", "c"];
 const emojis = ["✔️", "✨", "👀", "😭", "👎"];
+
+const form = document.querySelector(".quiz-form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const results = [];
+
+  const radioButtons = document.querySelectorAll("input[type='radio']:checked");
+
+  radioButtons.forEach((radioButton, index) => {
+    if (radioButton.value === responses[index]) {
+      results.push(true);
+    } else {
+      results.push(false);
+    }
+  });
+  console.log(results);
+
+  showResults(results);
+}
+
+const titleResult = document.querySelector(".show-results h2");
+const mark = document.querySelector(".mark");
+const help = document.querySelector(".help");
+
+function showResults(results) {}
