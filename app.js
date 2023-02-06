79a1de4a08e380fa18923d77750bec5b2e544133
joseph_dev <joseph.schneider0105@gmail.com>
2023-02-06 18:21:05 -0500

    js terminé, mais pas corrigé

--- app.js
+++ app.js
@@ -21,10 +21,94 @@ function handleSubmit(e) {
   console.log(results);
 
   showResults(results);
+  addColors(results);
 }
 
-const titleResult = document.querySelector(".show-results h2");
-const mark = document.querySelector(".mark");
-const help = document.querySelector(".help");
+const titleResult = document.querySelector(".show-result h2");
+const markResult = document.querySelector(".mark");
+const helpResult = document.querySelector(".help");
 
-function showResults(results) {}
+function showResults(results) {
+  const errorsNumber = results.filter((el) => el === false).length;
+  console.log(errorsNumber);
+
+  switch (errorsNumber) {
+    case 0:
+      titleResult.textContent = `✔️ Bravo, c'est un sans faute ! ✔️`;
+      helpResult.textContent = "Quelle culture ...";
+      helpResult.style.display = "block";
+      markResult.innerHTML = "Score : <span>5 / 5</span>";
+      markResult.style.display = "block";
+      break;
+    case 1:
+      titleResult.textContent = `✨ Vous y êtes presque ! ✨`;
+      helpResult.textContent =
+        "Retentez une autre réponse dans la case rouge, puis re-validez !";
+      helpResult.style.display = "block";
+      markResult.innerHTML = "Score : <span>4 / 5</span>";
+      markResult.style.display = "block";
+      break;
+    case 2:
+      titleResult.textContent = `✨ Encore un effort ... 👀`;
+      helpResult.textContent =
+        "Retentez une autre réponse dans les cases rouges, puis re-validez !";
+      helpResult.style.display = "block";
+      markResult.innerHTML = "Score : <span>3 / 5</span>";
+      markResult.style.display = "block";
+      break;
+    case 3:
+      titleResult.textContent = `👀 Il reste quelques erreurs. 😭`;
+      helpResult.textContent =
+        "Retentez une autre réponse dans les cases rouges, puis re-validez !";
+      helpResult.style.display = "block";
+      markResult.innerHTML = "Score : <span>2 / 5</span>";
+      markResult.style.display = "block";
+      break;
+    case 4:
+      titleResult.textContent = `😭 Peut mieux faire ! 😭`;
+      helpResult.textContent =
+        "Retentez une autre réponse dans les cases rouges, puis re-validez !";
+      helpResult.style.display = "block";
+      markResult.innerHTML = "Score : <span>1 / 5</span>";
+      markResult.style.display = "block";
+      break;
+    case 5:
+      titleResult.textContent = `👎 Peut mieux faire ! 👎`;
+      helpResult.style.display = "block";
+      helpResult.textContent =
+        "Retentez une autre réponse dans les cases rouges, puis re-validez !";
+      markResult.style.display = "block";
+      markResult.innerHTML = "Score : <span>0 / 5</span>";
+      break;
+
+    default:
+      titleResult.textContent = "Woops, cas innatendu.";
+  }
+}
+
+const questions = document.querySelectorAll(".question-block");
+
+function addColors(results) {
+  results.forEach((response, index) => {
+    if (results[index]) {
+      questions[index].style.backgroundImage =
+        "linear-gradient(to right, #a8ff78, #78ffd6";
+    } else {
+      questions[index].style.backgroundImage =
+        "linear-gradient(to right, #f5567b, #fd674c";
+    }
+  });
+}
+
+const radioInputs = document.querySelectorAll("input[type= 'radio']");
+radioInputs.forEach((radioInput) =>
+  radioInput.addEventListener("input", resetColor)
+);
+
+function resetColor(e) {
+  const index = e.target.getAttribute("name").slice(1) - 1;
+  const parentQuestionBlock = questions[index];
+
+  parentQuestionBlock.style.backgroundColor = "#f1f1f1";
+  parentQuestionBlock.style.backgroundImage = "none";
+}
